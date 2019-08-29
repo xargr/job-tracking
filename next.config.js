f0696e9ca5c11c6303ebc1e82fb50653e3fbc874
xargr <xargrigoris@gmail.com>
2019-08-29 17:41:05 +0200

    fix broken urls

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const withCSS = require('@zeit/next-css');
+const assetsUrl = process.env.NODE_ENV === 'production' ? '/job-tracking/' : '';
 
 module.exports = withCSS({
   cssModules: true,
-  distDir: 'build'
+  distDir: 'build',
+  assetPrefix: assetsUrl,
 });
