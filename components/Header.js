ec53f5bee3b889d0fb48af40ee3515671a2ccdb9
xargr <xargrigoris@gmail.com>
2019-09-16 00:10:32 +0200

    conditional header buttons

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
+import { JobContext } from './context/JobContext';
 import Export from './Button/Export';
 import DeleteAll from './Button/DeleteAll';
 import Import from './Button/Import';
@@ -32,19 +33,28 @@ const RightSide = styled.div`
   margin-top: 30px;
 `;
 
-const Header = () => (
-  <Container>
-    <LeftSide>
-      <H1>Job Tracking</H1>
-      <Span>
-        Stay organized by tracking the companies and jobs you are applying to.{' '}
-      </Span>
-    </LeftSide>
-    <RightSide>
-      <Import />
-      <DeleteAll />
-      <Export />
-    </RightSide>
-  </Container>
-);
+const Header = () => {
+  const { jobs } = useContext(JobContext);
+  const jobsCount = jobs && Object.keys(jobs).length;
+
+  return (
+    <Container>
+      <LeftSide>
+        <H1>Job Tracking</H1>
+        <Span>
+          Stay organized by tracking the companies and jobs you are applying to.{' '}
+        </Span>
+      </LeftSide>
+      <RightSide>
+        <Import />
+        {jobsCount > 0 && (
+          <>
+            <DeleteAll />
+            <Export />
+          </>
+        )}
+      </RightSide>
+    </Container>
+  );
+};
 export default Header;
